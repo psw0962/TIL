da1f6fcd08367043c881d88f95598fc92845da59
psw0962 <65061325+psw0962@users.noreply.github.com>
2020-08-03 21:40:10 +0900

    자료구조 연습

---
+++ prac.js
@@ -0,0 +1,26 @@
+const fruits = ['banana', 'apple', 'mango', 'papaya', 'orenge'];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// };
+
+// console.log(fruits.pop());
+// console.log(fruits);
+
+// fruits.push('kiwi');
+// console.log(fruits);
+
+// console.log(fruits.shift());
+// console.log(fruits);
+
+// console.log(fruits.unshift('kiwi'));
+// console.log(fruits);
+
+// console.log(fruits.indexOf('banana'));
+// console.log(fruits.indexOf('papaya'));
+
+// console.log(fruits.splice(2, 2));
+// console.log(fruits);
+
+// console.log(fruits.slice(0, 2));
+// console.log(fruits);
